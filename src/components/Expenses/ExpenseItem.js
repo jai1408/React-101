3bb4395fe2ac206572db89c6e2fb4e6ba217e397
jai1408 <jaishankarmishra17@gmail.com>
2021-05-22 23:23:41 +0530

    react101: event handling onClick of button, expenseTitle change

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,12 +3,20 @@ import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  let title = props.title;
+  //this clickHandler is not updating title because React is only rendering its component once.
+  //so we will use react hooks `useState`
+  const clickHandeler = () => {
+    title = "Updated Value!";
+    console.log(title);
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
+        <button onClick={clickHandeler}>Click me!</button>
       </div>
     </Card>
   );
