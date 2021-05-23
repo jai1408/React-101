ca97cc62afb24bb021d6be2cf561dd5258337ae3
jai1408 <jaishankarmishra17@gmail.com>
2021-05-23 15:31:33 +0530

    react101: useState React Hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,26 @@
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
+import React, { useState } from "react";
 
 const ExpenseItem = (props) => {
-  let title = props.title;
+  /**
+   * ? why to use const below for title & setTitle
+   * ! because we are not changing the value by assigning, we are changing value by set method.
+   */
+  const [title, setTitle] = useState(props.title); //destructuring Modern JS
+  //let title = props.title;
   //this clickHandler is not updating title because React is only rendering its component once.
   //so we will use react hooks `useState`
   const clickHandeler = () => {
-    title = "Updated Value!";
-    console.log(title);
+    //title = "Updated Value!";
+    setTitle("Updated Value!");
+    /*
+    * ! React hook useState don't support callbacks
+    setTitle("Updated Value!", () => {
+    console.log(this.state.name);
+    });*/
+    //console.log(title);
   };
   return (
     <Card className="expense-item">
@@ -23,3 +35,31 @@ const ExpenseItem = (props) => {
 };
 
 export default ExpenseItem;
+
+/**
+ * * state example in Class based Component
+ * class ExpenseItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: props.title;
+    };
+  }
+
+  const clickHandeler = () => {
+    this.setState({ 
+      title: 'Updated Value!'; 
+    }
+  )}
+
+  render() {
+    return (
+      <div>
+        <button onClick={clickHandler}>
+          Click me
+        </button>
+      </div>
+    );
+  }
+}
+ */
